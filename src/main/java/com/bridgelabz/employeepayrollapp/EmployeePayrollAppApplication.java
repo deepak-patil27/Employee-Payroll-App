99fdb5ab60c19aa2e9ec15db1e872b1d3e4a0fe3
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-09-02 10:15:34 +0530

    Modified: The Logging Levels,Logging To Console Or file,Logging Patterns

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,14 +1,21 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
         System.out.println("Welcome to  Employee Payroll App Devlopment");
         SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll Service Application Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
     }
 
 }
